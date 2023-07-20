5fe92ef85d90d1c1cfa006c4d75bf665c8068819
Nathan <nathan@naykel.com.au>
2023-07-20 14:05:11 +1000

    house keeping

--- week1/task2.js
+++ week1/task2.js
@@ -22,9 +22,14 @@ console.log(numsArray);
 numsArray = numsArray.map(num => num * 2);
 console.log(numsArray);
 
-// The first parameter of reduce is a callback function that takes two arguments: the accumulator and the currentValue.
-// In each iteration, the callback function adds the currentValue to the accumulator.
-// The second parameter of reduce is the initial value of the accumulator, which is set to 0 in this case.
+// The first parameter of reduce is a callback function that takes two
+// arguments: the accumulator and the currentValue.
+
+// In each iteration, the callback function adds the currentValue to the
+// accumulator.
+
+// The second parameter of reduce is the initial value of the accumulator,
+// which is set to 0 in this case.
 
 let arrayTotal = numsArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0 );
 

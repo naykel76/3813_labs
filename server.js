a8a07f5277879c7df9cc38638bf98f7601b592b3
Nathan <nathan@naykel.com.au>
2023-07-29 13:45:01 +1000

    directory restructure

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 // Require the Express module and create an Express application
 const express = require('express');
 const app = express();
-
+const path = require('path');
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 // Create an HTTP server using the Express application
 const http = require('http').Server(app);
 
-// Serve static files from the 'www' directory
-// (replace 'www' with your directory name)
-app.use(express.static(__dirname + '/www'));
-
-// Start the server to listen on port 3000
-let server = http.listen(3000, function () {
-    // const host = "localhost";
-
-    // Get the server's listening address and the port number
+// Start the server to listen on port 3008
+let server = http.listen(3008, function () {
     let host = server.address().address;
     let port = server.address().port;
-
-    console.log('NodeJS Server');
-    console.log(`Server listening on: ${host} port: ${port}`);
+    console.log(`NodeJS Server listening on: ${host} port: ${port}`);
+    console.log('http://localhost:' + port);
 });
+
+app.get('/account', function (res, res) {
+    res.sendFile(__dirname + '/public/account.html');
+})
+
+// app.post('/login', function (res, res) {
+//     res.sendFile(__dirname + '/public/index.html');
+// })
+
